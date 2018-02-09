271f04267406bea88166629fb6d242ef058c2487
RF <RF@Admin>
2018-02-09 18:44:22 +0800

    20180209 18:44提交

--- idCard/src/com/idCard/action/IDQueryAction.java
+++ idCard/src/com/idCard/action/IDQueryAction.java
@@ -1,3 +1,6 @@
+/*
+ * Copyright (C) 2017 Baidu, Inc. All Rights Reserved.
+ */
 package com.idCard.action;
 
 import java.net.URLEncoder;
@@ -7,38 +10,29 @@ import com.idCard.utils.FileUtil;
 import com.idCard.utils.HttpUtil;
 
 /**
- * 
- * ���֤ʶ�𹤾���
- * 
- * @author cjx
- * 
- *
+ * 身份证识别
  */
-
 public class IDQueryAction {
-
-	/**
-	 * ���֤ʶ��
-	 */
 
 	public static String getIDInfo(String filePath) {
 
-		// ���֤ʶ��url
+		// 身份证识别url
 		String idcardIdentificate = "https://aip.baidubce.com/rest/2.0/ocr/v1/idcard";
 		String result = null;
-		// ����ͼƬ·��
-		// String filePath = "D:\\360Downloads\\ID\\images\\idc.jpg";
+		// 本地图片路径
+		// String filePath = "D:/MyProject/idCard/WebContent/images/1.png";
 		try {
 			byte[] imgData = FileUtil.readFileByBytes(filePath);
 			String imgStr = Base64Util.encode(imgData);
-			// ʶ�����֤����id_card_side=front;ʶ�����֤����id_card_side=back;
+			// 识别身份证正面id_card_side=front;识别身份证背面id_card_side=back;
 			String params = "id_card_side=front&" + URLEncoder.encode("image", "UTF-8") + "="
 					+ URLEncoder.encode(imgStr, "UTF-8");
 			/**
-			 * ���ϻ���access_token�й���ʱ�䣬 �ͻ��˿����л��棬���ں����»�ȡ��
+			 * 线上环境access_token有过期时间， 客户端可自行缓存，过期后重新获取。
 			 */
 			String accessToken = AccessTokenAction.getAuth();
 			result = HttpUtil.post(idcardIdentificate, accessToken, params);
+			System.out.println(result);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -46,25 +40,25 @@ public class IDQueryAction {
 	}
 
 	/**
-	 * ��Ҫ��ʾ���������蹤���� FileUtil,Base64Util,HttpUtil,GsonUtils���
+	 * 重要提示代码中所需工具类 FileUtil,Base64Util,HttpUtil,GsonUtils请从
 	 * https://ai.baidu.com/file/658A35ABAB2D404FBF903F64D47C1F72
 	 * https://ai.baidu.com/file/C8D81F3301E24D2892968F09AE1AD6E2
 	 * https://ai.baidu.com/file/544D677F5D4E4F17B4122FBD60DB82B3
-	 * https://ai.baidu.com/file/470B3ACCA3FE43788B5A963BF0B625F3 ����
+	 * https://ai.baidu.com/file/470B3ACCA3FE43788B5A963BF0B625F3 下载
 	 */
 	public static void main(String[] args) {
-		// ���֤ʶ��url
+		// 身份证识别url
 		String idcardIdentificate = "https://aip.baidubce.com/rest/2.0/ocr/v1/idcard";
-		// ����ͼƬ·��
-		String filePath = "D:\\360Downloads\\ID\\images\\idc.jpg";
+		// 本地图片路径
+		String filePath = "D:/MyProject/idCard/WebContent/images/1.png";
 		try {
 			byte[] imgData = FileUtil.readFileByBytes(filePath);
 			String imgStr = Base64Util.encode(imgData);
-			// ʶ�����֤����id_card_side=front;ʶ�����֤����id_card_side=back;
+			// 识别身份证正面id_card_side=front;识别身份证背面id_card_side=back;
 			String params = "id_card_side=front&" + URLEncoder.encode("image", "UTF-8") + "="
 					+ URLEncoder.encode(imgStr, "UTF-8");
 			/**
-			 * ���ϻ���access_token�й���ʱ�䣬 �ͻ��˿����л��棬���ں����»�ȡ��
+			 * 线上环境access_token有过期时间， 客户端可自行缓存，过期后重新获取。
 			 */
 			String accessToken = AccessTokenAction.getAuth();
 			String result = HttpUtil.post(idcardIdentificate, accessToken, params);
@@ -73,5 +67,4 @@ public class IDQueryAction {
 			e.printStackTrace();
 		}
 	}
-
 }
