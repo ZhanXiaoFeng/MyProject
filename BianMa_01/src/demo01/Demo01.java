c57e4947fbb53c97cde0ed67235f3535f85e5f92
ZhanXiaoFeng <444591845@qq.com>
2018-06-20 16:34:41 +0800

    201806201633Demo01_不要在常量和变量中出现易混淆的字母

---
+++ BianMa_01/src/demo01/Demo01.java
@@ -0,0 +1,16 @@
+package demo01;
+
+/*
+ * 不要在常量和变量中出现易混淆的字母
+ * Time:20180620
+ * Author:ZRF
+ */
+
+public class Demo01 {
+
+	public static void main(String[] args) {
+		long i = 1l;
+		System.out.println("i 的两倍是： " + (i + i));
+	}
+
+}
