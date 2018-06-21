06478c721446206871ddb17c05f4b188d7711c57
ZhanXiaoFeng <444591845@qq.com>
2018-06-21 22:07:34 +0800

    201806212207Commit_不要只替换一个类

---
+++ BianMa_01/src/demo20/Constant.java
@@ -0,0 +1,24 @@
+
+/**  
+* @Title: Constant.java
+* @Package demo20
+* @Description: TODO(用一句话描述该文件做什么)
+* @author ZRF
+* @date 2018年6月21日
+* @version V1.0  
+*/
+
+package demo20;
+
+/**
+    * @ClassName: Constant
+    * @Description: TODO(这里用一句话描述这个类的作用)
+    * @author ZRF
+    * @date 2018年6月21日
+    */
+
+public class Constant {
+	// 定义人类寿命极限
+	public final static int MAX_AGE = 180;
+
+}
---
+++ BianMa_01/src/demo20/Demo20.java
@@ -0,0 +1,25 @@
+
+/**  
+* @Title: Demo20.java
+* @Package demo20
+* @Description: 不要只替换一个类
+* @author ZRF
+* @date 2018年6月21日
+* @version V1.0  
+*/
+
+package demo20;
+
+/**
+ * @ClassName: Demo20
+ * @Description: 不要只替换一个类
+ * @author ZRF
+ * @date 2018年6月21日
+ */
+
+public class Demo20 {
+	public static void main(String[] args) {
+		System.out.println(" 人类寿命极限是： " + Constant.MAX_AGE);
+	}
+
+}
