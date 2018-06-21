7f9492112f3009687cdf6a6e2a5d2c5eb83e4088
ZhanXiaoFeng <444591845@qq.com>
2018-06-21 12:17:27 +0800

    201806211217_不要让旧语法困扰你

---
+++ BianMa_01/src/demo08/Demo08.java
@@ -0,0 +1,35 @@
+
+/**  
+* @Title: Demo08.java
+* @Package demo08
+* @Description: 不要让旧语法困扰你
+* @author ZRF
+* @date 2018年6月21日
+* @version V1.0  
+*/
+
+package demo08;
+
+/**
+ * @ClassName: Demo08
+ * @Description: 不要让旧语法困扰你
+ * @author ZRF
+ * @date 2018年6月21日
+ */
+
+public class Demo08 {
+	public static void main(String[] args) {
+		// 数据定义及初始化
+		int fee = 200;
+		// 其他业务处理
+		saveDefault: save(fee);
+		// 其他业务处理
+	}
+
+	static void saveDefault() {
+	}
+
+	static void save(int fee) {
+	}
+
+}
