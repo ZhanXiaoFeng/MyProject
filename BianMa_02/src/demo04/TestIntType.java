05fb6c201ac29c29703a0f66af8e44c07cd6611f
ZhanXiaoFeng <444591845@qq.com>
2018-06-22 14:15:02 +0800

    201806221414Commit_测试Int类型的最大值

---
+++ BianMa_02/src/demo04/TestIntType.java
@@ -0,0 +1,27 @@
+
+/**  
+* @Title: TestIntType.java
+* @Package demo04
+* @Description: 测试Int类型的最大值
+* @author ZRF
+* @date 2018年6月22日
+* @version V1.0  
+*/
+
+package demo04;
+
+/**
+ * @ClassName: TestIntType
+ * @Description: 测试Int类型的最大值
+ * @author ZRF
+ * @date 2018年6月22日
+ */
+
+public class TestIntType {
+	public static void main(String[] args) {
+		int i = 2147483647;
+		i++;
+		System.out.println(i);
+	}
+
+}
