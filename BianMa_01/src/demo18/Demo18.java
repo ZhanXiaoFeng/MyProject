7cdf2c654c9e6650f9d357a17291ad7983129445
ZhanXiaoFeng <444591845@qq.com>
2018-06-23 11:08:47 +0800

    201806231108Commit_避免 instanceof 非预期结果

--- BianMa_01/src/demo18/Demo18.java
+++ BianMa_01/src/demo18/Demo18.java
@@ -36,7 +36,7 @@ public class Demo18 {
 		boolean b5 = null instanceof String;
 		System.out.println(b5);
 		// 类型转换后的空对象是否是 String 的实例
-		boolean b6 = (String) null instanceof String;
+		boolean b6 = (String)null instanceof String;
 		System.out.println(b6);
 		// Date 对象是否是 String 的实例30 编写高质量代码： 改善 Java 程序的 151 个建议
 		// boolean b7 = new Date() instanceof String;
