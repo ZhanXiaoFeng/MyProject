431c10fa845b9715ab1285c88dd6dac7b5c6ba81
ZhanXiaoFeng <444591845@qq.com>
2018-06-23 11:31:23 +0800

    201806231126Commit_在接口中不要存在实现代码

---
+++ BianMa_03/src/demo01/Demo01.java
@@ -0,0 +1,23 @@
+
+    /**  
+    * @Title: Demo01.java
+    * @Package demo01
+    * @Description: TODO(用一句话描述该文件做什么)
+    * @author ZRF
+    * @date 2018年6月23日
+    * @version V1.0  
+    */
+    
+package demo01;
+
+
+    /**
+    * @ClassName: Demo01
+    * @Description: TODO(这里用一句话描述这个类的作用)
+    * @author ZRF
+    * @date 2018年6月23日
+    */
+
+public class Demo01 {
+
+}
