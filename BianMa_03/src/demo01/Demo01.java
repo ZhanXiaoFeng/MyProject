fd7fa5bf75ce77516f1b7d2260b24fec7c132bb9
ZhanXiaoFeng <444591845@qq.com>
2018-06-23 11:32:01 +0800

    201806231131Commit_在接口中不要存在实现代码

--- BianMa_03/src/demo01/Demo01.java
+++ BianMa_03/src/demo01/Demo01.java
@@ -1,23 +1,39 @@
 
-    /**  
-    * @Title: Demo01.java
-    * @Package demo01
-    * @Description: TODO(用一句话描述该文件做什么)
-    * @author ZRF
-    * @date 2018年6月23日
-    * @version V1.0  
-    */
-    
+/**  
+* @Title: Demo01.java
+* @Package demo01
+* @Description: 在接口中不要存在实现代码
+* @author ZRF
+* @date 2018年6月23日
+* @version V1.0  
+*/
+
 package demo01;
 
-
-    /**
-    * @ClassName: Demo01
-    * @Description: TODO(这里用一句话描述这个类的作用)
-    * @author ZRF
-    * @date 2018年6月23日
-    */
+/**
+ * @ClassName: Demo01
+ * @Description: 在接口中不要存在实现代码
+ * @author ZRF
+ * @date 2018年6月23日
+ */
 
 public class Demo01 {
+	public static void main(String[] args) {
+		// 调用接口的实现
+		B.s.doSomething();
+	}
+}
 
+// 在接口中存在实现代码
+interface B {
+	public static final S s = new S() {
+		public void doSomething() {
+			System.out.println("我在接口中实现了 ");
+		}
+	};
+}
+
+// 被实现的接口
+interface S {
+	public void doSomething();
 }
